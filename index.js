977737dfb392c25ddc763ece83df5deba403ab3f
Quince Lewis <106302729+quinceosanya@users.noreply.github.com>
2022-06-21 09:35:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books
+then((resp) => resp.json())
+then((json) => renderBooks(data));
   
 }
+renderBooks(books);
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +20,6 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+eturn fetch("https://anapioficeandfire.com/api/books
+then((resp) => resp.json())
+then((json) => renderBooks(data));
